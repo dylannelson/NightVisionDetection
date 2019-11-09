f79ede03140537df6383b644206d1d05b25afc9b
Dylan Nelson <dylannelson5454@gmail.com>
2019-11-09 14:20:58 -0800

    Finished Parsing script

--- Data/Videos/FrameTaker.py
+++ Data/Videos/FrameTaker.py
@@ -1,8 +1,29 @@
 import cv2
 import os
 VideosDir = os.getcwd()
-print("Should be Videos: ")
-print(VideosDir)
-print("Should be Images: ")
-ImagesDir = os.chdir('..')
-print(ImagesDir)
+os.chdir("../Images/")
+ImagesDir = os.getcwd()
+os.chdir("../Videos/")
+
+nthFrame = 75 # interval between selected frames to downloaded
+def FrameCapture(fileName):
+    path = VideosDir + "\\" + fileName # Converts file name into a path
+    vidObj = cv2.VideoCapture(path) # Opens the video
+    count = 0 # Current Frame
+    success = 1 # Manages whether we are past/on the last frame
+    while success:
+        for i in range(nthFrame):
+            # Skipping Frames
+            success, image = vidObj.read()
+            count += 1
+
+        # Saves the frames with frame-count
+        cv2.imwrite("../Images//" + fileName[:-4]+ "-Frame%d.jpg" % count, image)
+        print("Frame %d written" % count)
+    # Delete the last image, which is usually null
+    os.remove("../Images//" + fileName[:-4]+ "-Frame%d.jpg" % count)
+
+print("What file do you want to get frames from?")
+fileInput = input("File Name: ")
+FrameCapture(fileInput)
+print("FINISHED")
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # NightVisionDetection
+
+Completed:
+Python Script To Parse and Organize Frames from Videos 11/9/19
+-MP4 files in Data/Videos can be turned into images for annotating
+  and are placed in 'Images' folder by running the script in 'Video' File
+Next:
+Annotating Images
+Training Model
+Testing Model
+
+
+Sources:
+https://towardsdatascience.com/creating-your-own-object-detector-ad69dda69c85
+Videos:
+https://www.youtube.com/watch?v=zze3ynmxyYU
+https://www.youtube.com/watch?v=LcyyFlxC6iA
